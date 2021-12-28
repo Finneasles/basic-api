65ce5701c80a073040052d81c3be0b0bf328ca55
Finneas <85114582+Finneasles@users.noreply.github.com>
2021-12-28 02:37:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-// Data
-data = ["do","re","me","fa","so","la","ti","do"];
+// Example Static Data
+var StaticData = [
+    {
+        "id": "1",
+        "content": {
+        "entry":"Doo"
+    }
+    },
+    {
+        "id": "2",
+        "content": {
+        "entry":"Ree"
+    }
+    },
+    {
+        "id": "3",
+        "content": {
+        "entry":"Mee"
+    }
+    }
+];
 
-app.get("/", (req, res, next) => {res.json(data)});
+app.get("/", (req, res, next) => {res.json(StaticData)});
 app.listen(PORT, () => {console.log("Listening on port: "+ PORT)});
